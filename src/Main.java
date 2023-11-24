1194612523c6fd9969819db0b17d06b739e5f248
Francis0023 <150805817+Francis0023@users.noreply.github.com>
2023-11-24 11:40:11 -0500

    version1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("*****Bienvenido*****");
+        System.out.println(" --- Ingreso de datos de los estudiantes --- ");
+        Sistema estudiante1 = new Sistema("Mario","Sociales",7,25);
+        Sistema estudiante2 = new Sistema("Luis","Matematicas",8,75);
+        Sistema estudiante3 = new Sistema("Mario","Lenguaje",7,02);
+        Sistema estudiante4 = new Sistema("Mario","Ingles",9,24;
+
+        estudiante1.ImprimirDatos();
+
 
     }
 }
