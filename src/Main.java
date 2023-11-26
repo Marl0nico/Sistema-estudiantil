2584de77dd8e97911783e9d13f9b661bc04811c1
Francis0023 <150805817+Francis0023@users.noreply.github.com>
2023-11-26 17:07:24 -0500

    version3.0

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("*****Bienvenido*****");
         Sistema[] estudiantes = new Sistema[4];
+        double SumaNotas = 0;
+        System.out.println("*****Bienvenido*****");
         for (int i = 0; i < estudiantes.length; i++) {
             System.out.println("Ingrese los datos del estudiante " + (i + 1));
             System.out.print("Nombre: ");
@@ -15,12 +16,16 @@ public class Main {
             double nota = scanner.nextDouble();
 
             estudiantes[i] = new Sistema(nombre, materia, nota);
+            SumaNotas+=nota;
         }
+
+        //Promedio del curso
+        double PromCurso= SumaNotas/ estudiantes.length;
+        System.out.println("Promedio del curso: " + PromCurso);
+
         //Calcular promedio y Verificar si paso
         for (Sistema estudiante : estudiantes) {
             estudiante.mostrarDatos();
-            System.out.println("Promedio: " + estudiante.getNota());
-
             if (estudiante.getNota() > 7) {
                 System.out.println("El estudiante ha pasado.");
             } else {
