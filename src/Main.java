8bc0065e518bbf1347290d6bd1b056b54bea8e50
Marl0nico <marlon.nicolalde@epn.edu.ec>
2023-11-26 20:46:07 -0500

    Versión final
    Variable n para definir la cantidad de estudiantes que se quiere ingresar la información.
    Código para calcular si aprueba, supletorio o reprueba

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,36 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Sistema[] estudiantes = new Sistema[4];
-        double SumaNotas = 0;
+        System.out.print("Ingrese la cantidad de estudiantes que desea registrar: ");
+        int n=scanner.nextInt();
+        Sistema[] estudiantes = new Sistema[n];
+        double SumaNotas=0;
         System.out.println("*****Bienvenido*****");
-        for (int i = 0; i < estudiantes.length; i++) {
-            System.out.println("Ingrese los datos del estudiante " + (i + 1));
+        for (int i=0; i<estudiantes.length; i++){
+            System.out.println("Ingrese los datos del estudiante "+(i+1));
             System.out.print("Nombre: ");
-            String nombre = scanner.next();
+            String nombre=scanner.next();
             System.out.print("Materia: ");
-            String materia = scanner.next();
+            String materia=scanner.next();
             System.out.print("Nota: ");
-            double nota = scanner.nextDouble();
-
-            estudiantes[i] = new Sistema(nombre, materia, nota);
+            double nota=scanner.nextDouble();
+            estudiantes[i]=new Sistema(nombre, materia, nota);
             SumaNotas+=nota;
         }
-
         //Promedio del curso
-        double PromCurso= SumaNotas/ estudiantes.length;
-        System.out.println("Promedio del curso: " + PromCurso);
-
+        double PromCurso=SumaNotas/estudiantes.length;
+        System.out.println("Promedio del curso: "+PromCurso);
         //Calcular promedio y Verificar si paso
-        for (Sistema estudiante : estudiantes) {
+        for(Sistema estudiante:estudiantes){
             estudiante.mostrarDatos();
-            if (estudiante.getNota() > 7) {
-                System.out.println("El estudiante ha pasado.");
+            if (estudiante.getNota()>7){
+                System.out.println("El estudiante ha aprobado.");
+            } else if (estudiante.getNota()>5 && estudiante.getNota()<7){
+                System.out.println("El estudiante debe rendir supletorio.");
             } else {
-                System.out.println("El estudiante no ha pasado.");
+                System.out.println("El estudiante ha reprobado");
             }
-
-            System.out.println();
         }
-
     }
 }
--- src/Sistema.java
+++ src/Sistema.java
@@ -1,37 +1,31 @@
-public class Sistema {
+public class Sistema{
     public String nombre;
     public String materia;
     public double nota;
-
-    public Sistema(String nombre, String materia, double nota) {
-        this.nombre = nombre;
-        this.materia = materia;
-        this.nota = nota;
+    public Sistema(String nombre, String materia, double nota){
+        this.nombre=nombre;
+        this.materia=materia;
+        this.nota=nota;
     }
-
-    public String getNombre() {
+    public String getNombre(){
         return nombre;
     }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setNombre(String nombre){
+        this.nombre=nombre;
     }
-
-    public String getMateria() {
+    public String getMateria(){
         return materia;
     }
-
-    public void setMateria(String materia) {
-        this.materia = materia;
+    public void setMateria(String materia){
+        this.materia=materia;
     }
-
-    public double getNota() {
+    public double getNota(){
         return nota;
     }
-    public void setNota(double nota) {
-        this.nota = nota;
+    public void setNota(double nota){
+        this.nota=nota;
     }
-    public void mostrarDatos() {
-        System.out.println("Nombre:" + getNombre() + "  Materia:" + getMateria() + "    Nota:" + getNota());
+    public void mostrarDatos(){
+        System.out.println("Nombre:"+getNombre()+"  Materia:"+getMateria()+"    Nota:"+getNota());
     }
 }
